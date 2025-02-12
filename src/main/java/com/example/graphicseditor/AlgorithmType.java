107117de6e11a33a9c9d8c8504762fcd25932a4f
User <daniilpototskiy@mail.ru>
2025-02-12 18:06:00 +0300

    Added classes algorithm/shape selection

---
+++ src/main/java/com/example/graphicseditor/AlgorithmType.java
@@ -0,0 +1,7 @@
+package com.example.graphicseditor;
+
+public enum AlgorithmType {
+    DDA,
+    BRESENHAM
+    // В будущем можно добавить другие алгоритмы, например, Ву для отрезков.
+}
---
+++ src/main/java/com/example/graphicseditor/ShapeType.java
@@ -0,0 +1,7 @@
+package com.example.graphicseditor;
+
+public enum ShapeType {
+    LINE,
+    CIRCLE
+    // В будущем можно добавить другие фигуры, например, прямоугольник, эллипс и т.д.
+}
