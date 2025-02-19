90c69691614d880a0905abc69bfeece0ef2ea563
User <daniilpototskiy@mail.ru>
2025-02-19 18:38:13 +0300

    Added ability drawing circles

--- src/main/java/com/example/graphicseditor/EditorPanel.java
+++ src/main/java/com/example/graphicseditor/EditorPanel.java
@@ -14,6 +14,9 @@ public class EditorPanel extends JPanel {
     private final List<Point> currentLine = new ArrayList<>();
     private final List<List<Pixel>> allWuLines = new ArrayList<>();
     private final List<Pixel> currentWuLine = new ArrayList<>();
+    private final List<List<Point>> allCircles = new ArrayList<>();
+    private final List<Point> currentCircle = new ArrayList<>();
+
     private Point startPoint = null;
     private ShapeType shapeType = ShapeType.LINE;
     private AlgorithmType algorithmType = AlgorithmType.DDA;
@@ -41,6 +44,7 @@ public class EditorPanel extends JPanel {
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g;
 
+        g2d.setColor(Color.BLACK);
         for (List<Point> line : allLines) {
             for (int i = 0; i < line.size() - 1; i++) {
                 Point p1 = line.get(i);
@@ -54,6 +58,7 @@ public class EditorPanel extends JPanel {
             Point p2 = currentLine.get(i + 1);
             g2d.drawLine(p1.x, p1.y, p2.x, p2.y);
         }
+
         for (List<Pixel> line : allWuLines) {
             for (Pixel pixel : line) {
                 float brightness = pixel.getBrightness();
@@ -68,6 +73,12 @@ public class EditorPanel extends JPanel {
             g2d.fillRect(pixel.getX(), pixel.getY(), 1, 1);
         }
 
+        g2d.setColor(Color.BLACK);
+        for (List<Point> circle : allCircles) {
+            for (Point p : circle) {
+                g2d.fillRect(p.x, p.y, 1, 1);
+            }
+        }
     }
 
     private void drawShape(Point start, Point end) {
@@ -101,10 +112,10 @@ public class EditorPanel extends JPanel {
     }
 
     private void drawCircle(Point center, Point edge) {
-        currentLine.clear();
+        currentCircle.clear();
         int radius = (int) Math.sqrt(Math.pow(edge.x - center.x, 2) + Math.pow(edge.y - center.y, 2));
-        currentLine.addAll(CircleAlgorithm.drawCircleBresenham(center.x, center.y, radius));
-        allLines.add(new ArrayList<>(currentLine));
+        currentCircle.addAll(CircleAlgorithm.drawCircleBresenham(center.x, center.y, radius));
+        allCircles.add(new ArrayList<>(currentCircle));
     }
 
     public void setShapeType(ShapeType shapeType) {
